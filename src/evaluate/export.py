194e9643ded3f33094fb733d503a0378cbfcd26c
primbs <primbs@fim.uni-passau.de>
2024-03-27 11:34:40 +0000

    implemented abstract method

--- src/evaluate/export.py
+++ src/evaluate/export.py
@@ -62,6 +62,9 @@ class Exporter(abc.ABC):
 
 
 class ConsoleExporter(Exporter):
+    def export_abstract_predictions(self, references: list[str], top_k_predictions: list[list[str]]) -> None:
+        pass
+
     def export_predictions(
         self, references: list[str], top_k_predictions: list[list[str]]
     ) -> None:
